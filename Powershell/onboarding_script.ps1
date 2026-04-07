a45e9e50e2feb65e7f5ed90cd77249b8208518d5
Chris Eben <37830396+ashuchris@users.noreply.github.com>
2026-04-07 11:36:50 -0400

    Add files via upload

---
+++ Powershell/onboarding_script.ps1
@@ -0,0 +1,21 @@
+$users = Import-Csv "C:\Scripts\users.csv"
+
+foreach ($user in $users) {
+    # 1. Gather the data from the CSV row
+    $fullname = "$($user.FirstName) $($user.LastName)"
+    $username = $user.Username
+    $ouPath   = "OU=$($user.OU),DC=yourdomain,DC=local"
+    $targetGroup = $user.Group  # <--- New variable for the group name
+
+    # 2. Create the User
+    New-ADUser `
+        -Name $fullname `
+        -SamAccountName $username `
+        -Path $ouPath `
+        -AccountPassword (ConvertTo-SecureString "Password123!" -AsPlainText -Force) `
+        -Enabled $true
+
+    # 3. Add the User to the Security Group
+    # We use the $username we just created and the $targetGroup from the CSV
+    Add-ADGroupMember -Identity $targetGroup -Members $username
+}
